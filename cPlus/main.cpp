3800c4e9a7bc1384d78e27077dfe8325b0472008
Kwokc233 <1765074429@qq.com>
2023-07-18 10:41:44 +0800

    Initial Commit

---
+++ cPlus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cPlus
+//
+//  Created by kwok on 2023/7/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
